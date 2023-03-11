b9dbac404f0fb5f91eb7af91fd5ce864132dd76f
Evandro Rodrigues Coelho <evandrorodcoelho@gmail.com>
2023-03-11 16:15:12 -0300

    Style: Add global Style

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,11 @@
 import {RouterProvider } from "react-router-dom";
 import { router } from "./routes/routes";
+import { GlobalStyles } from "./styles/global";
 export function App() {
     return (
         <>
             <RouterProvider router={router}/>
+            <GlobalStyles  />
         </>
     );
 }
